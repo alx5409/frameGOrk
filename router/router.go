1dbb05df5f32c5642063acf5919f735b28ea984f
Alex <alx5409@gmail.com>
2026-04-15 11:24:44 +0200

    Add documentation in the router package

--- router/router.go
+++ router/router.go
@@ -10,10 +10,12 @@ import (
 	"github.com/alx5409/frameGOrk/utils"
 )
 
+// Stores all registered routes and serves as the HTTP entry point for request dispatch.
 type Router struct {
 	routes []*route.Route
 }
 
+// Adds a new route to the Router for the given method and path.
 func (r *Router) Register(method route.Method, path route.Path, handler route.HandlerFunc, middlewares ...middleware.Middleware) error {
 	finalHandler := middleware.Chain(handler, middlewares...)
 	newRoute := &route.Route{
@@ -25,10 +27,13 @@ func (r *Router) Register(method route.Method, path route.Path, handler route.Ha
 	return nil
 }
 
+// Returns all routes currently registered in the router .
 func (r *Router) GetRoutes() []*route.Route {
 	return r.routes
 }
 
+// Finds the handler that matches the given method and path. It returns the handler and true when found, otherwise nil
+// and false
 func HTTPDispatch(method route.Method, path route.Path, routes []*route.Route) (route.HandlerFunc, bool) {
 	for _, r := range routes {
 		if r.Method == method && r.Path == path {
@@ -38,6 +43,7 @@ func HTTPDispatch(method route.Method, path route.Path, routes []*route.Route) (
 	return nil, false
 }
 
+// Returns the distincts HTTP methods registered for a specific path. It is used to build the Allow header for 405 responses
 func allowedMethodsForPath(path route.Path, routes []*route.Route) []string {
 	seen := map[string]struct{}{}
 	methods := []string{}
@@ -53,6 +59,7 @@ func allowedMethodsForPath(path route.Path, routes []*route.Route) []string {
 	return methods
 }
 
+// It dispatches requests by method and path, executes the matched handler and handles 404 and 405 errors.
 func (r *Router) ServeHTTP(w http.ResponseWriter, req *http.Request) {
 	method := route.Method(utils.NormalizeMethod(req.Method))
 	path := route.Path(req.URL.Path)
